7d74c4a7ea387847a3694bb90b33e901919107da
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-01-23 15:36:37 +0900

    복습

--- 알고리즘_문제풀이_복습.js
+++ 알고리즘_문제풀이_복습.js
@@ -1,1 +1,11 @@
+document.querySelector('.button1').addEventListener('click', function(){
+  let a = parseInt(document.querySelector('.number1-add1').value);
+  let b = parseInt(document.querySelector('.number1-add2').value);
 
+  document.querySelector('.number1-result1-1').value = a + b;
+  document.querySelector('.number1-result1-2').value = a - b;
+  document.querySelector('.number1-result1-3').value = a * b;
+  document.querySelector('.number1-result1-4').value = a / b;
+  document.querySelector('.number1-result1-5').value = a % b;
+
+});
